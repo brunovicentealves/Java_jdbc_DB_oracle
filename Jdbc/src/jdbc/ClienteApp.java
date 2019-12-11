c322486c716da104d9144f6788e73cbc1892df9e
Bruno Alves <brunovicentealves.ti@gmail.com>
2019-12-11 18:41:04 -0200

    criando o crud jdbc

--- Jdbc/src/jdbc/ClienteApp.java
+++ Jdbc/src/jdbc/ClienteApp.java
@@ -2,10 +2,14 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
 
 public class ClienteApp {
-	
+
 	static String url = "jdbc:oracle:thin:@localhost:1521:XE";
 	static String usuario = "system";
 	static String senha = "admin";
@@ -17,7 +21,54 @@ public class ClienteApp {
 		conexao = DriverManager.getConnection(url, usuario, senha);
 		conexao.setAutoCommit(false);
 	}
-	
+
+	public static void desconectar() throws SQLException {
+
+		conexao.close();
+	}
+
+	public static void inserir(int id_pessoa , String nome,String cpf) throws SQLException {
+		
+		String sql = "insert into PESSOA values("+id_pessoa+",'"+nome+",'"+cpf+"')";
+		Statement statement = conexao.createStatement();
+		statement.execute(sql);
+		conexao.commit();
+		
+	}
+
+	public static void consultar(int id_pessoa) throws SQLException {
+		String sql = "select * from PESSOA where id_pessoa ="+id_pessoa+"";
+		Statement statement = conexao.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		
+		while (rs.next()) {
+
+			JOptionPane.showMessageDialog(null,
+					"cpf :" + rs.getInt(1) + "Nome :" + rs.getString(2) + "Cpf :" + rs.getString(3));
+		}
+
+	}
+
+	public static void consultarTodosRegistros() throws SQLException {
+		
+		String sql = "select * from PESSOA ";
+		Statement statement = conexao.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		
+		while (rs.next()) {
+
+			JOptionPane.showMessageDialog(null,
+					"cpf :" + rs.getInt(1) + "Nome :" + rs.getString(2) + "Cpf :" + rs.getString(3));
+		}
+
+
+	}
+
+	public static void alterar() {
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
